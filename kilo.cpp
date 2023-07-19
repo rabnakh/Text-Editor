b5ba434f68a3414731c5abdde3a1b89b8432631d
rabnakh <xurizo903@gmail.com>
2023-07-19 16:31:35 -0500

    Added miscellaneous flags

--- kilo.cpp
+++ kilo.cpp
@@ -25,19 +25,22 @@ void enableRawMode(){
 	// ICRNL translates carriage return '13' into new line '10'
 	// IXON stops data from being sent to terminal when Ctrl-S is
 	// ...pressed until Ctrl-Q is pressed
-	raw.c_iflag &= ~(ICRNL | IXON);
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 
 	// OPOST translates \n into \r\n
 	raw.c_oflag &= ~(OPOST);
 
+	raw.c_cflag |= (CS8);
+
 	// ECHO causes each key to be printed to the terminal.
 	// ICANON enables canonical mode. Turning it off will all to read 
-	// ...input byte by byte rather than line by line
+	// ...input byte by byte rather than line by line to the program
 	// IEXTEN allows for sending a character literally after pressing 
 	// ...Ctrl-V
 	// ISIG enables Ctrl-C and Ctrl-Z to send their signals to the 
 	// ...process
 	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
 
 	// Apply the modified terminal attributes.
 	tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
@@ -52,7 +55,7 @@ int main(){
 	// Returns 0 when reached end of file or 'q' is reached
 	// Characters left unread wil remain in the input queue
 	while(read(STDIN_FILENO,&c,1) == 1 && c != 'q'){
-	
+
 		// Tests if a character is a control character
 		// Added \r to printf() because OPOST was turned off
 		if(iscntrl(c)){
@@ -62,5 +65,6 @@ int main(){
 			printf("%d ('%c')\r\n",c,c);
 		}
 	}
+
 	return 0;
 }
